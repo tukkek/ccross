280371a24c75e1e3676ffbf0d7253c6691ebc2ed
Alex Henry <tukkek@gmail.com>
2025-01-19 00:00:00 -0300

    Original deck.

--- CREDITS.md
+++ CREDITS.md
@@ -1,2 +1,1 @@
-* back.jpg @ https://commons.wikimedia.org/wiki/File:Tarot_nouveau_-_Grimaud_-_1898_-_Back_side.jpg
 * background.webp https://pixabay.com/illustrations/wood-texture-dark-background-old-4036454/
--- src/deck.js
+++ src/deck.js
@@ -1,33 +1,43 @@
-export var cards=[]
+export var cards=[
+  '01 storm.png',
+  '02 gale.png',
+  '03 calm.png',
+  '04 gust.png',
+  '05 zephyr.png',
+  '06 ash.png',
+  '07 ember.png',
+  '08 kindling.png',
+  '09 pyre.png',
+  '10 hearth.png',
+  '11 desert.png',
+  '12 oasis.png',
+  '13 mountain.png',
+  '14 forest.png',
+  '15 jungle.png',
+  '16 misery.png',
+  '17 poverty.png',
+  '18 struggle.png',
+  '19 comfort.png',
+  '20 abundance.png',
+  '21 hell.png',
+  '22 umbra.png',
+  '23 eden.png',
+  '24 astral.png',
+  '25 heaven.png'
+]
 
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864
-function randomize(array){
-  var currentIndex = array.length, temporaryValue, randomIndex;
+export function shuffle(){
+  var currentIndex = cards.length, temporaryValue, randomIndex;
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
     // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
   }
-  return array;
-}
-
-export async function shuffle(){
-  let index=await fetch('./tarot/index.xml')
-  index=await index.text()
-  index=new DOMParser().parseFromString(index,'text/xml')
-  cards.push(...index.querySelectorAll('card'))
-  randomize(cards)
-  let descriptions=await fetch('./tarot/PictorialKey.xml')
-  descriptions=await descriptions.text()
-  descriptions=new DOMParser().parseFromString(descriptions,'text/xml')
-  descriptions=Array.from(descriptions.querySelectorAll('card'))
-  for(let c of cards){
-    c.description=descriptions.find(d=>d.id==c.id).querySelectorAll('paragraph')
-    c.description=Array.from(c.description).map(d=>d.innerHTML).join('\n\n')
-  }
+  return cards;
 }
